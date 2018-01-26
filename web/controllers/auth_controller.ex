7fbc35baac92979e4b20087799d845f497200cbf
mikesaprykin <mikesaprykin@gmail.com>
2018-01-26 11:25:49 +0200

    feat(plugs, auth): Added auth plug to add user if it is logged in

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -15,6 +15,12 @@ defmodule Discuss.AuthController do
     |> signin(conn)
   end
 
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: topic_path(conn, :index))
+  end
+
   defp insert_or_update_user(changeset) do
     case Repo.get_by(User, email: changeset.changes.email) do
       nil ->
--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -28,8 +29,10 @@ defmodule Discuss.Router do
   scope "/auth", Discuss do
     pipe_through :browser
 
+    get "/signout", AuthController, :signout
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
+    
   end
 
   # Other scopes may use custom stacks.

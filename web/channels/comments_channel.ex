339196e9bbb59016a94cead2d7cedbb4ecccb9b7
mikesaprykin <mikesaprykin@gmail.com>
2018-01-28 10:18:13 +0200

    feat(socket, channels): Added socket comnfigurations and comments channel to the app

---
+++ web/channels/comments_channel.ex
@@ -0,0 +1,14 @@
+defmodule Discuss.CommentsChannel do
+  use Discuss.Web, :channel
+
+  def join(name, _params, socket) do
+    IO.puts "++++++"
+    IO.puts name
+    {:ok, %{hey: "there"}, socket}
+  end
+
+  def handle_in do
+
+  end
+
+end
